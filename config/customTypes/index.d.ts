c2720dd4548af467cb656e7451388cae7cd4e9c3
mankowsj <jmankowsk@gmail.com>
2020-02-22 20:06:55 +0100

    tsconfig types update

--- config/customTypes/index.d.ts
+++ config/customTypes/index.d.ts
@@ -1,12 +1,3 @@
-declare type DynamicImport<C> = () => Promise<C>;
-
-declare type PageNavigationName = 'mainpage' | 'credits' | 'settings';
-
-interface CMap<T> {
-  [key: string]: T;
-}
-
-// declaration.d.ts
 declare module '*.css' {
   const content: any;
   export default content;
@@ -22,19 +13,7 @@ declare module '*.svg' {
   export default content;
 }
 
-interface Empty {}
-
-type CProps = {
-  props: {
-    [key: string]: any;
-  };
-};
-
-type PickProps<A extends CProps, B extends keyof A['props']> = {
-  [K in B]: A['props'][K];
-};
-
-type ExtractProps<A extends CProps, B extends keyof A['props']> = A['props'][B];
+type ReduxImage = {path: string; name: string};
 
 //
 // declare module '*.css' {
