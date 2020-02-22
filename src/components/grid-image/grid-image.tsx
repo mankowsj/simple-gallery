112e38e9dccb1e41dce87542b975fdeff7efa184
Jerzy Mańkowski <jmankowsk@gmail.com>
2020-02-22 12:07:59 +0100

    basic grid with mock

---
+++ src/components/grid-image/grid-image.tsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import './grid-image.styles.pcss';
+
+type GridImageProps = {
+  className: string;
+  imageName: string;
+  imageSrc: string;
+};
+const GridImage = ({className, imageName, imageSrc}: GridImageProps) => {
+  const [hovered, setHover] = useState(false);
+
+  return (
+    <div className={`${className} grid-image vertical-fix`}>
+      <img
+        className={hovered ? 'primary-light_border-color' : ''}
+        src={imageSrc}
+        onMouseOver={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      />
+    </div>
+  );
+};
+GridImage.defaultProps = {
+  className: ''
+};
+
+export {GridImage};
--- src/views/gallery/gallery.tsx
+++ src/views/gallery/gallery.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import './gallery.styles.pcss';
+import {GridImage} from '../../components/grid-image';
+
+const pathPrefix = 'src/assets/images/';
+const imageList = ['1', '2', '3', '4', '5', '6', '7', '8'];
 
 type GalleryProps = {
   className: string;
 };
 const Gallery = ({className}: GalleryProps) => (
-  <section className={className}>gallery</section>
+  <section className={`${className} gallery`}>
+    {imageList.map(imgName => (
+      <GridImage
+        key={imgName}
+        className="grid-item"
+        imageName={imgName}
+        imageSrc={`${pathPrefix}${imgName}.jpg`}
+      />
+    ))}
+  </section>
 );
 Gallery.defaultProps = {
   className: ''
