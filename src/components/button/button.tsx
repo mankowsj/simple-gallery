732b73876363c5a7077f87888e564f5e31b1a941
mankowsj <jmankowsk@gmail.com>
2020-02-22 14:19:00 +0100

    cleanup

--- src/components/button/button.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-type ButtonProps = {
-  onClick: React.MouseEventHandler;
-};
-export const Button = ({onClick}: ButtonProps) => (
-  <button onClick={onClick}>Add</button>
-);
