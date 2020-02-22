58b41b4af793c5e79be69af78d9d44ad16b6e8c0
Jerzy Mańkowski <jmankowsk@gmail.com>
2020-02-22 10:35:49 +0100

    global styles setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import {App} from './views/app';
+import './global-styles/index.pcss';
 
 ReactDom.render(<App />, document.querySelector('#app'));
