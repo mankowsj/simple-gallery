4132c72052a34f549d94b2a7e56c96a9c91fee45
mankowsj <jmankowsk@gmail.com>
2020-02-22 16:34:10 +0100

    show animation

--- src/views/big-picture/big-picture.tsx
+++ src/views/big-picture/big-picture.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
 import './big-picture.styles.pcss';
 import {IconButton} from '../../components/icon-button';
@@ -9,10 +9,35 @@ type BigPictureProps = {
   className?: string;
   setGalleryMode: any;
 };
+
+const VisiblityMap = {
+  NotVisible: 0,
+  Visible: 1,
+  Exiting: 2
+};
+
+const getVisibilityModifier = (state: number) => {
+  switch (state) {
+    case VisiblityMap.Visible: {
+      return 'is-visible';
+    }
+    case VisiblityMap.Exiting: {
+      return '';
+    }
+  }
+  return '';
+};
+
 const BigPicture = ({className, setGalleryMode}: BigPictureProps) => {
+  const [isVisible, setVisibility] = useState(0);
+
+  useEffect(() => {
+    setVisibility(1);
+  }, []);
+
   return (
     <main className={`${className} gallery`}>
-      <section className="big-picture">
+      <section className={`big-picture ${getVisibilityModifier(isVisible)}`}>
         <nav>
           <IconButton
             onClick={() => {
