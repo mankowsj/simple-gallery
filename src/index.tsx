a510ccd683ba882936ea8801aa5effee3d1b8f40
mankowsj <jmankowsk@gmail.com>
2020-02-22 13:12:42 +0100

    moved provider to index

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,13 @@ import 'picturefill';
 import React from 'react';
 import ReactDom from 'react-dom';
 import {App} from './views/app';
+import {Provider} from 'react-redux';
+import {store} from './redux';
 import './global-styles/index.pcss';
 
-ReactDom.render(<App />, document.querySelector('#app'));
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#app')
+);
--- src/views/app/app.tsx
+++ src/views/app/app.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import {Provider} from 'react-redux';
-import {store} from '../../redux';
 import {Header} from '../header';
 import {Gallery} from '../gallery';
 import {Footer} from '../footer';
 import './app.styles.pcss';
 
 export const App = () => (
-  <Provider store={store}>
-    <div className="app theme-light">
-      <Header />
-      <Gallery className="grow" />
-      <Footer />
-    </div>
-  </Provider>
+  <div className="app theme-light">
+    <Header />
+    <Gallery className="grow narrow" />
+    <Footer />
+  </div>
 );
