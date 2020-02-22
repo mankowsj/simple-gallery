10a035ae2f417361fbf3567fac0c8ec25364d268
mankowsj <jmankowsk@gmail.com>
2020-02-22 20:07:42 +0100

    basic small slider pic setup

--- src/components/slider-small-pic/slider-small-pic.tsx
+++ src/components/slider-small-pic/slider-small-pic.tsx
@@ -1,3 +1,19 @@
 import React from 'react';
+import './slider-small-pic.styles.pcss';
 
-export const SliderSmallPic = () => <span>slider-smallPic</span>;
+type SliderSmallPicProps = {
+  className?: string;
+  onClick?: React.MouseEventHandler;
+  image: ReduxImage;
+};
+
+const SliderSmallPic = ({onClick, className}: SliderSmallPicProps) => (
+  <div onClick={onClick} className={`${className} slider-small-pic`}></div>
+);
+
+SliderSmallPic.defaultProps = {
+  onClick: () => {},
+  className: ''
+};
+
+export {SliderSmallPic};
