af960dec05cb5237005df106f6a4f6a31fc2d63a
mankowsj <jmankowsk@gmail.com>
2020-02-22 20:07:18 +0100

    basic slider setup

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -1,3 +1,28 @@
 import React from 'react';
+import './slider.styles.pcss';
+import {SliderSmallPic} from '../slider-small-pic';
 
-export const Slider = () => <span>test</span>;
+type SliderProps = {
+  images: ReduxImage[];
+  selectedIndex: number;
+  className?: string;
+  onChange?: () => void;
+};
+
+const reduceImages = (list: ReduxImage[], selectedIndex: number) => {
+  return list;
+};
+
+const Slider = ({className, images, selectedIndex}: SliderProps) => (
+  <div className={`${className} slider`}>
+    {reduceImages(images, selectedIndex).map(image => (
+      <SliderSmallPic className="slider-item" image={image} key={image.name} />
+    ))}
+  </div>
+);
+Slider.defaultProps = {
+  className: '',
+  onChange: () => {}
+};
+
+export {Slider};
