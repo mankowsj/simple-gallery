711a7f0ace7f30c9dbb0165e3d515bc072d9de45
mankowsj <jmankowsk@gmail.com>
2020-02-23 12:34:54 +0100

    webpack/ts: alias cleanup

--- webpack.resolve.js
+++ webpack.resolve.js
@@ -4,12 +4,9 @@ module.exports = {
   resolve: {
     modules: ['node_modules', 'assets/customTypes'],
     alias: {
-      scripts: path.resolve('./src/scripts'),
-      forms: path.resolve('./src/components/forms'),
-      common: path.resolve('./src/components/common'),
-      layout: path.resolve('./src/components/layout'),
-      base: path.resolve('./src/components/base'),
-      svg: path.resolve('./src/svg'),
+      '@components': path.resolve('./src/components'),
+      '@redux': path.resolve('./src/redux'),
+      svg: path.resolve('./src/svg')
     },
     extensions: ['.ts', '.tsx', '.js']
   }
