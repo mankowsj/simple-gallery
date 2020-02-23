e423f2d90d96e3e673c8851955cdae589361010d
mankowsj <jmankowsk@gmail.com>
2020-02-23 14:46:21 +0100

    mixins applied; proper switch position

--- src/views/header/header.tsx
+++ src/views/header/header.tsx
@@ -9,11 +9,13 @@ type HeaderProps = {
 const Header = ({className}: HeaderProps) => (
   <header className="secondary-light">
     <div className="whitening">
-      <section className="narrow vertical-fix">
-        <span className={`${className} header-icon`} />
-        <span>Simple Gallery</span>
+      <section className="narrow header-content">
+        <div className="left vertical-fix">
+          {/* <span className={`${className} header-icon`} /> */}
+          <span>Simple Gallery</span>
+        </div>
 
-        <div className="controls">
+        <div className="controls vertical-fix">
           <Switch labels={['Light mode', 'Dark mode']} />
         </div>
       </section>
