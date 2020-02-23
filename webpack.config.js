45f7e7e33c42cc304f9b24a41ffe624d62f82f19
mankowsj <jmankowsk@gmail.com>
2020-02-23 12:34:17 +0100

    webpack: config cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
 var path = require('path'),
-    resolve = require('./webpack.resolve').resolve,
-    rules = require('./config/webpack/rules'),
-    plugins = require('./config/webpack/plugins'),
-    devServer = require('./config/webpack/dev-server'),
-    entry = require('./config/webpack/entries');
+  resolve = require('./webpack.resolve').resolve,
+  rules = require('./config/webpack/rules'),
+  plugins = require('./config/webpack/plugins'),
+  devServer = require('./config/webpack/dev-server'),
+  entry = require('./config/webpack/entries');
 
 module.exports = {
-    devtool: false,
-    target: 'web',
-    entry,
-    output: {
-        filename: '[name].js',
-        chunkFilename: '[name].chunk.js',
-        pathinfo: true,
-        path: path.join(__dirname, './build')
-    },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                styles: {
-                    name: 'styles',
-                    test: /\.css$/,
-                    chunks: 'all',
-                    enforce: true
-                }
-            }
-        }
-    },
-    devServer,
-    resolve,
-    plugins,
-    module: {
-        rules
-    }
+  devtool: false,
+  target: 'web',
+  entry,
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[name].chunk.js',
+    pathinfo: true,
+    path: path.join(__dirname, './build')
+  },
+  // optimization: {
+  //     splitChunks: {
+  //         cacheGroups: {
+  //             styles: {
+  //                 name: 'styles',
+  //                 test: /\.css$/,
+  //                 chunks: 'all',
+  //                 enforce: true
+  //             }
+  //         }
+  //     }
+  // },
+  devServer,
+  resolve,
+  plugins,
+  module: {
+    rules
+  }
 };
