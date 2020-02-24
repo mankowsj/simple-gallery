5c72c224931257a81ba13166fb1cd2935e7d4f6d
mankowsj <jmankowsk@gmail.com>
2020-02-24 12:02:28 +0100

    action-button: outlinefocus more readable, active animation

--- src/components/action-button/action-button.tsx
+++ src/components/action-button/action-button.tsx
@@ -11,7 +11,11 @@ type ActionButtonProps = React.ComponentProps<typeof IconButton> & {
 const getColorStyle = (colors: string[] | undefined) => (colors ? {color: colors[0], background: colors[1]} : {});
 
 const ActionButton = ({colors, onClick, style, className, label, ...rest}: ActionButtonProps) => (
-  <span onClick={onClick} style={{...getColorStyle(colors), ...style}} className={`${className} action-button`}>
+  <span
+    tabIndex={0}
+    onClick={onClick}
+    style={{...getColorStyle(colors), ...style}}
+    className={`${className} action-button`}>
     {label ? <label>{label}</label> : false}
     <IconButton {...rest} />
   </span>
