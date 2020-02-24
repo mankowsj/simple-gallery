93bfef52c4ea7959efb7666bf5722ddca17dba91
mankowsj <jmankowsk@gmail.com>
2020-02-24 18:35:09 +0100

    media query selectors fix

--- config/webpack/plugins.js
+++ config/webpack/plugins.js
@@ -1,15 +1,20 @@
 var Utils = require('./utils'),
-    HtmlWebpackPlugin = require('html-webpack-plugin'),
-    MiniCssExtractPlugin = require('mini-css-extract-plugin');
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+  ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 
 module.exports = [
-    new HtmlWebpackPlugin({
-        template: Utils.fromRoot('config/htmlTemplates/index.ejs'),
-        inject: true
-    }),
-    new MiniCssExtractPlugin({
-        filename: '[name].st.css',
-        chunkFilename: '[name].[id].css',
-        allChunks: true,
-    })
+  new HtmlWebpackPlugin({
+    template: Utils.fromRoot('config/htmlTemplates/index.ejs'),
+    inject: true,
+    meta: {viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'}
+  }),
+  new ScriptExtHtmlWebpackPlugin({
+    defaultAttribute: 'defer'
+  }),
+  new MiniCssExtractPlugin({
+    filename: '[name].st.css',
+    chunkFilename: '[name].[id].css',
+    allChunks: true
+  })
 ];
